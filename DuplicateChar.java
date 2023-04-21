abe7d196c35271329d8317874a58b53ce12375cf
Pradeep SL <121848914+Pradeep-SL@users.noreply.github.com>
2023-04-21 16:25:55 +0530

    Add files via upload

---
+++ DuplicateChar.java
@@ -0,0 +1,71 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+class DuplicateChar
+{
+    // public static void main(String[] args) 
+    // {
+    //   String str="AA BB CC CC";
+    //    System.out.println(getDuplicate(str));
+
+    // }
+    // public static String getDuplicate(String str) 
+    // {
+      
+    //     String []words=str.split(" ");
+    //     StringBuffer sbr=new StringBuffer();
+    //     HashSet<String> set=new HashSet<>();
+       
+    //         for (String word :words ) 
+    //         { 
+    //           if(!set.contains(word))
+    //           {
+    //             set.add(word);
+    //             sbr.append(word).append(" ");
+    //           }
+    //         }
+    //       return sbr.toString();
+    
+    // }
+    // public static void main(String[] args) {
+    //   String str="helloo";
+    //    StringBuffer sb=new StringBuffer();
+    //    for (int i = 0; i < str.length(); i++) 
+    //    {
+    //       char ch=str.charAt(i); 
+    //       int idx=str.indexOf(ch,i+1);
+    //       if(idx==-1)
+    //       {
+    //         sb.append(ch);
+    //       }
+    //    }
+    //    System.out.println(sb);
+    // }
+//     public static String replaceString(String str, String target, String replacement) {
+//       StringBuilder sb = new StringBuilder();
+//       int i = 0;
+//       int j = 0;
+      
+//       while ((j = str.indexOf(target, i)) >= 0) {
+//           sb.append(str.substring(i, j));
+//           sb.append(replacement);
+//           i = j + target.length();
+//       }
+      
+//       sb.append(str.substring(i));
+//       return sb.toString();
+//   }
+//   public static void main(String[] args) {
+//       String input = "hello world";
+//       String target = "o";
+//       String replacement = "p";
+//       String output = replaceString(input, target, replacement);
+//       System.out.println(output); // prints "hello everyone"
+
+//   }
+public static void main(String[] args) 
+{
+    String Str="pradeep"    
+}
+}
